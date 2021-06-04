3776a937f55a7e6b1491f9d6f5c08b2311ccd5ab
Muhammad Essa <iMuhammadessa@gmail.com>
2021-06-04 13:06:47 +0500

    App.js file modified

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ var sub = x - y;
 // Multiplication program
 var mul = x * y;
 
-console.log(sum);
-console.log(sub);
-console.log(mul);
+// console.log(sum);
+// console.log(sub);
+// console.log(mul);
+
+// Log Modules exported
+var myLogModule = require('./Log.js');
+myLogModule.info('Node.js started');
+
+// Message Module exported
+var msg = require('./Messages.js');
+console.log(msg);
+
+// Data Module exported
+var person = require('./data.js');
+console.log(person.firstName + ' ' + person.lastName);
